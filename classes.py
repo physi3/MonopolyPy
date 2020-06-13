fd49a4596f3e7b49957319686963806f536a7e3a
joemarriage <45903919+joemarriage@users.noreply.github.com>
2020-06-13 17:35:10 +0100

    Add player, board and property classes.

--- classes.py
+++ classes.py
@@ -1,15 +1,36 @@
 class Board:
-    pass
+    def __init__(self):
+        self.size = 0
+        self.spaces = []
+        self.players = []
 
 
 class Space:
-    name = ''
-    position = 0
+    def __init__(self):
+        self.name = ''
+        self.position = 0
 
 
 class Property(Space):
-    pass
+    def __init__(self):
+        self.colour = ""
 
 
 class Special(Space):
     pass
+
+class Player:
+    def __init__(self):
+        self.name = ""
+        self.position = 0
+        self.inJail = False
+
+    def move(self, board, displacement):
+        self.position += displacement
+        if self.position < 0:
+            self.position += board.size
+        if self.position >= board.size:
+            self.position -= board.size
+    
+    def moveDirectly(self, position):
+        self.position = position
