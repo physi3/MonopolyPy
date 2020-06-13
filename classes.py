f06a916e26f16d9ca62fde186f289f876757bbb0
joemarriage <45903919+joemarriage@users.noreply.github.com>
2020-06-13 22:41:00 +0100

    Add Chances, Community Chests and more player methods.

--- classes.py
+++ classes.py
@@ -1,5 +1,6 @@
 from csv import reader as csvreader
 from random import randint
+from io import StringIO
 
 class Board:
     def __init__(self, filepath="./boards/UK.board"):
@@ -7,29 +8,67 @@ class Board:
         spaces = []
 
         with open(filepath, 'r') as boardfile:
-            reader = csvreader(boardfile)
-            header = [*map(str.lower, next(reader))]
+            splitBoard = boardfile.read().split('&')
+            spacesCSV = StringIO(splitBoard[0])
+            chances = splitBoard[1].split("\n")[:-1]
+            community = splitBoard[2].split("\n")[:-1]
 
-            # Expected values: name, type, set.
-            values = ("name", "type", "set")
-            order = [header.index(s) for s in values]
-            for row in reader:
-                # Keyword access to data.
-                row = {value: row[i] for value, i in zip(values, order)}
-                # Check the type of space.
-                space = None
-                t = row["type"].lower()
-                if t == "property":
-                    space = Property(row["name"], row["set"])
-                elif t == "special":
-                    space = Special(row["name"])
-                else:
-                    raise Exception(f"'{t}' is an invalid type! ({filepath})")
+        reader = csvreader(spacesCSV)
+        header = [*map(str.lower, next(reader))]
 
-                # Add the space to the board.
-                spaces.append(space)
-            self.spaces = spaces
+        # Expected values: name, type, set.
+        values = ("name", "type", "set")
+        order = [header.index(s) for s in values]
+        for row in reader:
+            # Keyword access to data.
+            row = {value: row[i] for value, i in zip(values, order)}
+            # Check the type of space.
+            space = None
+            t = row["type"].lower()
+            if t == "property":
+                space = Property(row["name"], row["set"])
+            elif t == "special":
+                space = Special(row["name"])
+            else:
+                raise Exception(f"'{t}' is an invalid type! ({filepath})")
 
+            # Add the space to the board.
+            spaces.append(space)
+        self.spaces = spaces
+
+        self.chances = []
+        for i in chances:
+            self.chances.append(Card(i.split(",")[0],i.split(",")[1]))
+
+        self.community = []
+        for i in community:
+            self.community.append(Card(i.split(",")[0],i.split(",")[1]))
+
+
+class Card:
+    def __init__(self, message, function):
+        self.message = message
+        self.function = function
+    def __repr__(self):
+        return self.message+" "+self.function
+    def actFunction(self, player):
+        if "|" in function:
+            if function.split("|")[0] == "Money":
+                player.money+=int(function.split("|")[1])
+            elif function.split("|")[0] == "Move":
+                player.move(int(function.split("|")[1]))
+            elif function.split("|")[0] == "Directly":
+                player.moveDirectly(int(function.split("|")[1]),True)
+            elif function.split("|")[0] == "EachPlayer":
+                #get money from each player
+                pass
+        else:
+            if function == "Jail":
+                #go to jail
+                pass
+            elif function == "JailFree":
+                #getttoyu jail
+                pass
 
 class Space:
     name = ''
@@ -54,10 +93,13 @@ class Special(Space):
 
 class Player:
     def __init__(self):
-        self.name = ''
         self.position = 0
         self.inJail = False
         self.doubleCounter = 0
+        self.money = 0
+
+    def passGo(self):
+        self.money+=200
 
     def move(self, board, displacement):
         self.position += displacement
@@ -65,9 +107,13 @@ class Player:
             self.position += board.size
         if self.position >= board.size:
             self.position -= board.size
+            self.passGo()
 
-    def moveDirectly(self, position):
+    def moveDirectly(self, position, collectGo):
+        if self.position > position and collectGo:
+            self.passGo()
         self.position = position
+
 
     def rollDice():
         dice1 = randint(1,6)
@@ -77,3 +123,4 @@ class Player:
         return dice1+dice2
 
     def turn(self, board):
+        self.move(board,self.rollDice())
